c6c8d316f787de7cc3b7edd06d6de65ae9ef65b2
ElisaCarballo <elisa.carballo79@gmail.com>
2022-09-21 21:11:34 -0500

    header-completo

--- 04-react-auth/src/components/Header/Header.jsx
+++ 04-react-auth/src/components/Header/Header.jsx
@@ -1,11 +1,25 @@
 import './Header.scss'
 
-function App () {
+const Header = () => {
   return (
-    <>
-      <h1>header</h1>
-    </>
+    <nav className='header'>
+      <a href='/' className='header__logo'>Logo</a>
+      <ul className='header__nav-list'>
+        <li className='header__list-item'>
+          <a href='/' className='header__item-link header__item-link--is-active'>Home</a>
+        </li>
+        <li className='header__list-item'>
+          <a href='/login' className='header__item-link'>Login</a>
+        </li>
+        <li className='header__list-item'>
+          <a href='/signup' className='header__item-link'>signup</a>
+        </li>
+        <li className='header__list-item'>
+          <a href='/secret' className='header__item-link'>Secret</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
-export default App
+export default Header
