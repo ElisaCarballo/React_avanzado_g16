5df8ee581ee4f7039b806e71882685a048163631
ElisaCarballo <elisa.carballo79@gmail.com>
2022-09-21 21:34:05 -0500

    scss estyle

--- 04-react-auth/src/components/Header/Header.jsx
+++ 04-react-auth/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.scss'
 const Header = () => {
   return (
     <nav className='header'>
-      <a href='/' className='header__logo'>Logo</a>
+      <a href='/' className='header__logo'>LOGO</a>
       <ul className='header__nav-list'>
         <li className='header__list-item'>
           <a href='/' className='header__item-link header__item-link--is-active'>Home</a>
@@ -12,7 +12,7 @@ const Header = () => {
           <a href='/login' className='header__item-link'>Login</a>
         </li>
         <li className='header__list-item'>
-          <a href='/signup' className='header__item-link'>signup</a>
+          <a href='/signup' className='header__item-link'>Signup</a>
         </li>
         <li className='header__list-item'>
           <a href='/secret' className='header__item-link'>Secret</a>
