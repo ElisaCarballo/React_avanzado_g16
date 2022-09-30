4fbf289469f0a654e3e9fda8e443d279678a89ef
ElisaCarballo <elisa.carballo79@gmail.com>
2022-09-30 17:22:50 -0500

    proyecto

---
+++ ecommerce22/src/App.js
@@ -0,0 +1,11 @@
+import { BrouserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css'
+
+function App () {
+  return (
+    <div className='App' />
+    <h1>Navbar</h1>
+  )
+}
+
+export default App
